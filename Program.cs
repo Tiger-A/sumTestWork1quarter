00ca49b56195c51b170b9895f955bdad5d681927
Alexei Efremov <td-interface@yandex.ru>
2022-09-20 14:57:46 +0400

    add shem

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿        
+﻿        using static System.Console;
+Clear();
+
+
+string[] array1 = new string[6] {"Portland", "Bob", "say", "miss", "stay", "dog"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
